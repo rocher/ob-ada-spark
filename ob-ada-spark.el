e80920d4832d8e82d2d1c88aabb0b5f894a37310
Francesc Rocher <francesc.rocher@gmail.com>
2022-02-17 22:11:55 +0100

    Bump version to v1.0.1

--- ob-ada-spark.el
+++ ob-ada-spark.el
@@ -5,7 +5,7 @@
 ;; Author: Francesc Rocher
 ;; Keywords: Ada/SPARK, literate programming, reproducible research
 ;; Homepage: https://github.com/rocher/ob-ada-spark
-;; Version: 1.0.0
+;; Version: 1.0.1
 
 ;; This file is NOT part of GNU Emacs.
 
