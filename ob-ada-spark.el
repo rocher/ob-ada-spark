a10717fc6523c4c19b8ae9db6acd2443708b0d9b
Francesc Rocher <francesc.rocher@gmail.com>
2022-07-15 20:22:56 +0200

    Bump version to 1.3.0

--- ob-ada-spark.el
+++ ob-ada-spark.el
@@ -7,7 +7,7 @@
 ;; Keywords: languages, tools, outlines
 ;; URL: https://github.com/rocher/ob-ada-spark
 ;; Package-Requires: ((emacs "26.1") (f "0.20.0"))
-;; Version: 1.2.5c
+;; Version: 1.3.0
 
 ;; This file is NOT part of GNU Emacs.
 
